b95d6faaa2a1756aeb7e5385f9743f8f6e54bfaa
Emerson916 <emersons.a296@gmail.com>
2022-01-12 11:21:46 -0300

    Fazendo um dropdawn nas redes sociais mobile

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function menu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
+
+function menuLateral() {
+    const menuLateral = document.getElementById("menuLateral");
+    menuLateral.classList.toggle('active');
+}
